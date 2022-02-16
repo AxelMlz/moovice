830657515a2fa8ed059b64ba8a6fccccdfb4810a
AxelMlz <axel.fm.melezan@gmail.com>
2022-02-16 16:58:08 +0100

    Stage3

--- src/views/popular.js
+++ src/views/popular.js
@@ -12,8 +12,8 @@ export default class popular extends Component {
         // this.state.movies.push("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0d213053b4412c4badf1b0e0e9e5bda2")
         // console.log(this.state.movies)
         fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0d213053b4412c4badf1b0e0e9e5bda2`)
-        .then(res => res.json())
-        .then(res => { 
+        .then((res) => res.json())
+        .then((res) => { 
             this.setState({movies:res})
             console.log(this.state.movies);
         })  
